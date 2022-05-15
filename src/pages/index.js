5fe8d641857e06d8d89493b51b9717a480cbb3a8
Antonio Vivace <antonio.vivace@cern.ch>
2022-05-15 22:28:57 +0200

    style: set wrapping in flex containers so buttons can break lines

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <div>
           <Link
             className="button button--secondary button--lg btn-install"
             to="/install">
