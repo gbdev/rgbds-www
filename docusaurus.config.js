05c8e60f1dcb2654a297a63c990378f26408ea21
Antonio Vivace <avivace4@gmail.com>
2025-05-07 14:42:33 +0200

    config: announcement styling, improve text

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,8 +108,9 @@ const config = {
       ],
       announcementBar: {
         id: 'gbcompo25-1',
+        backgroundColor: '#FADFA1',
         content:
-          '📢 A new edition of the GB Compo kicks off on June 1st! Create a Game Boy game, music, tool, or demoscene ROM and compete for glory and awesome prizes! <a target="_blank" href="https://itch.io/jam/gbcompo25">Join now on Itch.io!</a>',
+          '📢 A new Game Boy coding competition kicks off on June 1st! Create a game, music, tool, or demoscene ROM and compete for glory and awesome prizes! <a target="_blank" href="https://itch.io/jam/gbcompo25">Join the GB Compo 25 now on Itch.io!</a>',
       },
       navbar: {
         title: "RGBDS",
